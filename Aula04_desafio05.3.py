21c8970916a0265e3a8db07d97859c3fd8b5f5cf
juan-m-cloud <85756801+juan-m-cloud@users.noreply.github.com>
2025-03-14 12:05:19 -0300

    Update Aula04_desafio05.3.py

--- Aula04_desafio05.3.py
+++ Aula04_desafio05.3.py
@@ -1,7 +1,10 @@
+banidos = ["João", "Maria", "Carlos"]  
 nome = str(input("Digite seu nome: "))
 idade = int(input("Digite sua idade: "))
-acomp = str(input("(Apenas para menores): Está acompanhado dos pais?: "))
-if idade >= 18 or acomp == "sim": 
-    print ("Bem-vindo a nossa festa!")
+acomp = str(input("(Apenas para menores): Está acompanhado dos pais?: ")) .lower()
+if nome in banidos:
+    print("Acesso negado! Você está na lista de banidos.")
+elif idade >= 18 or acomp == "sim":
+    print("Bem-vindo à nossa festa!")
 else:
     print("Acesso negado!")
